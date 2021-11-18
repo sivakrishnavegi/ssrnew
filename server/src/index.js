f43ec5540df66cd7954e472e34685a2ec3d7c7f3
sivakrishnavegi <sivakrishnavegi@yahoo.com>
2021-11-18 15:37:00 +0530

    refactor render file

--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,13 @@
 
 import express from 'express';
-import React from 'react';
-import { renderToString } from 'react-dom/server';
-import Home from './client/components/Home';
-
+import renderer from './helpers/renders/renderer';
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const content = renderToString(<Home />);
 
-  const html = `
-   <html>
-   <head>
-   <head>
-   <body>
-   <div id="root">
-   ${content}
-   </div>
-   <script src="bundle.js" /> 
-   </body>
-   </html>
-  `
-   
-  res.send(html);
+  res.send(renderer());
 })
 
 app.listen( 3000, () => {
